d81ae3571621bef79add90f48c33a8bf0eccce05
Zenit-yapper <zenitog69@gmail.com>
2026-04-11 12:06:17 +0530

    Create FullbrightMixin.java

---
+++ src/main/java/com/astreal/client/mixin/FullbrightMixin.java
@@ -0,0 +1,18 @@
+package com.astreal.client.mixin;
+
+import net.minecraft.client.option.SimpleOption;
+import net.minecraft.client.render.LightmapTextureManager;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(LightmapTextureManager.class)
+public class FullbrightMixin {
+    @Inject(method = "getBrightness", at = @At("HEAD"), cancellable = true)
+    private static void onGetBrightness(CallbackInfoReturnable<Float> cir) {
+        // Return 15.0f (Max Light) at all times
+        cir.setReturnValue(15.0f);
+    }
+}
+
