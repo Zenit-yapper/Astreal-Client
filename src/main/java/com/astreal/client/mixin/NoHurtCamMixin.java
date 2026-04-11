e3d071cd354c1d3d3c01f58604ab54fbdb46de41
Zenit-yapper <zenitog69@gmail.com>
2026-04-11 12:05:14 +0530

    Create NoHurtCamMixin.java

---
+++ src/main/java/com/astreal/client/mixin/NoHurtCamMixin.java
@@ -0,0 +1,17 @@
+package com.astreal.client.mixin;
+
+import net.minecraft.client.render.GameRenderer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(GameRenderer.class)
+public class NoHurtCamMixin {
+    @Inject(method = "tiltViewWhenHurt", at = @At("HEAD"), cancellable = true)
+    private void disableHurtTilt(CallbackInfo ci) {
+        // Cancels the view tilting effect entirely
+        ci.cancel();
+    }
+}
+
