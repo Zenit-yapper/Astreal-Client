3466b643f26479f6300ac268c80b44ff6d167d38
Zenit-yapper <zenitog69@gmail.com>
2026-04-11 12:43:08 +0530

    Update AstrealClient.java

--- src/main/java/com/astreal/client/AstrealClient.java
+++ src/main/java/com/astreal/client/AstrealClient.java
@@ -1,20 +1,46 @@
 package com.astreal.client;
 
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
-import com.astreal.client.mods.HUDManager; // We will put the drawing logic here
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import org.lwjgl.glfw.GLFW;
+
+import com.astreal.client.mods.HUDManager;
+import com.astreal.client.gui.AstrealClickGui;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class AstrealClient implements ClientModInitializer {
+    public static final String MOD_ID = "astreal-client";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    // 1. Create the Click GUI Keybinding (Right Shift)
+    private static KeyBinding configKey;
 
     @Override
     public void onInitializeClient() {
-        // This is the registration! 
-        // It tells the game: "Every time you draw the HUD, run my code."
-        HudRenderCallback.EVENT.register((drawContext, tickDelta) -> {
-            HUDManager.render(drawContext, tickDelta);
+        LOGGER.info("------------------------------------------");
+        LOGGER.info("ASTREAL CLIENT: Initializing PvP Engine...");
+        LOGGER.info("------------------------------------------");
+
+        // 2. Register the HUD (FPS, CPS, Armor durabilty)
+        HUDManager.init();
+
+        // 3. Setup the Keybind
+        configKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+                "key.astreal.settings", 
+                InputUtil.Type.KEYSYM, 
+                GLFW.GLFW_KEY_RIGHT_SHIFT, 
+                "category.astreal.client"
+        ));
+
+        // 4. Listen for the Key Press to open the Animated GUI
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            while (configKey.wasPressed()) {
+                client.setScreen(new AstrealClickGui());
+            }
         });
-        
-        System.out.println("Astreal Client HUD Initialized!");
     }
 }
-
